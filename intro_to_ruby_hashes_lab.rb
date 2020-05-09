20eedadcc521260d0cc825a1469cd9a0ec4df676
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-09 16:34:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ actor = {name: "dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = { 
+	monopoly = {
   railroads: {}
 }
 
@@ -16,7 +16,7 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 		railroads: {
-			pieces: 4, 
+			pieces: 4,
 			names: {}
 		}
 	}
