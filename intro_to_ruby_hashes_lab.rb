537159f1705a01bfa791e71d89b628b0331ae9d9
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-09 16:36:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,8 +20,8 @@ def monopoly_with_second_tier
 			names: {}
 		}
 	}
-end
-end#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+ end
+end #copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
 
